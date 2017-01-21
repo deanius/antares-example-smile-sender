bb115eaf659fbd419b8fafada649d245b022665c
Dean Radcliffe <deanmisc@yahoo.com>
2017-01-20 22:37:46 -0600

    Demo client/server Antares.announce examples
    let getShoutAction = (p)=>({type: 'Hello.World', payload: p})
    Antares.announce(getShoutAction, 42)
    
    /*
    AD (5281)> Hello.World  {
       payload: 42,
       meta: { antares: { actionId: 5281 } }
    }
    AP (5281)> Sending Hello.World upstream
    */
    
    let route = '/profile/23'
    let profileStamper = () => ({ profileId: Number(route.split('/')[2]) })
    let currentUser = () => 'user-100'
    let userStamper = () => ({ createdBy: currentUser() })
    
    Antares.announce(getShoutAction, {value: 27}, profileStamper)
    
    /*
    AD (2046)> Hello.World  {
      payload: { currentUser: '23', value: 27 },
      meta: { antares: { actionId: 2046 } }
    }
    AP (2046)> Sending Hello.World upstream
    */
    
    Antares.announce(getShoutAction, {value: 33}, profileStamper,
    userStamper)
    
    /*
    AD (9211)> Hello.World  {
      payload: { profileId: 23, value: 33 },
      meta: {
        antares:
          { actionId: 9211,
            createdBy: 'user-100',
            connectionId: null,
            key: undefined
          }
       }
    }
    AP (9211)> Sending Hello.World upstream
    */

--- imports/antares/main.js
+++ imports/antares/main.js
@@ -1,6 +1,13 @@
-import { AntaresMeteorInit, AntaresInit } from 'meteor/deanius:antares'
+import { AntaresMeteorInit, AntaresInit, inAgencyRun } from 'meteor/deanius:antares'
 // TODO elaborate on this config
 const AntaresConfig = {}
 
 // Make our initialized Antares available
 export const Antares = AntaresMeteorInit(AntaresInit)(AntaresConfig)
+
+// Expose debugging variables
+inAgencyRun('any', function () {
+    Object.assign(this, {
+        Antares
+    })
+})
