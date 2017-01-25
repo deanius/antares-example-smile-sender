96422a7c9691483272de91563d087a0955cca8ce
Dean Radcliffe <deanmisc@yahoo.com>
2017-01-24 18:20:43 -0600

    feat: UI will rerender upon actions. Buttons send smiles :)
    Elements are instances of components. Components are either ES6 or
    createClass style element-generators that may or may not keep local
    state. Ours do not need them.

--- imports/antares/reactRenderer.js
+++ imports/antares/reactRenderer.js
@@ -4,24 +4,44 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { mount } from 'react-mounter'
 
-const WelcomeComponent = ({name}) => (
+const eventHandlers = {
+    sendASmile: () => {
+        Antares.announce('surprise!')
+    }
+}
+
+const WelcomeComponent = ({ sendASmile }) => (
+    <div>
     <p>Ho Hum.</p>
-);
+    <button onClick={ sendASmile }>Send A Smile!</button>
+    </div>
+)
+
+const Smiler = ({ sendASmile }) => (
+    <div>
+        <div>Smile!</div>
+        <br/>
+        <button onClick={ sendASmile }>Send A Smile!</button>
+    </div>
+)
+
+const welcomeElement = <WelcomeComponent { ...eventHandlers } />
+const smilerElement = <Smiler { ...eventHandlers } />
 
 // ensure we have a #react-root upon load
-mount(()=>(<WelcomeComponent/>))
+mount(()=>(welcomeElement))
 
 // A function that can be run in order to change the contents of react-root
 // then change them back after a delay
 const reactRenderer = ({ action }) => {
     const reactRoot = document.getElementById('react-root')
-    ReactDOM.render(<div>Smile!</div>, reactRoot)
+    ReactDOM.render(smilerElement, reactRoot)
 
     // NOTE We can do better than this code which would need clearTimeouts
     // for certain cases. We'll use Observables with switchMap later..
     // But this'll work as long as we dont fire off events too quickly..
     setTimeout(() => {
-        ReactDOM.render(React.createElement(WelcomeComponent), reactRoot)
+        ReactDOM.render(welcomeElement, reactRoot)
     }, 2000)
 }
 
