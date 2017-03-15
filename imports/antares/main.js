09c7e1f8020dc1c314230bae3c4a6b7d5cb9b948
Dean Radcliffe <deanmisc@yahoo.com>
2017-03-15 16:24:51 -0500

    save to mongo (in Meteor)

--- imports/antares/main.js
+++ imports/antares/main.js
@@ -1,14 +1,19 @@
 import { AntaresMeteorInit, AntaresInit, inAgencyRun } from 'meteor/deanius:antares'
+import { Mongo } from 'meteor/mongo'
+
 // TODO elaborate on this config
 const AntaresConfig = {}
 
 // Make our initialized Antares available
 export const Antares = AntaresMeteorInit(AntaresInit)(AntaresConfig)
+const { mongoRendererFor } = Antares
+let Smiles = process.env.MONGO_URL && new Mongo.Collection('Smiles')
 
 // Expose debugging variables
 inAgencyRun('any', function () {
     Object.assign(this, {
-        Antares
+        Antares,
+        Smiles
     })
 })
 
@@ -27,6 +32,18 @@ inAgencyRun('server', () => {
                 key: 'Declan',
                 localOnly: true
             }
+        }
+    })
+})
+
+console.log('MONGO_URL', process.env.MONGO_URL)
+inAgencyRun('server', () => {
+    Antares.subscribeRenderer(({ action }) => {
+        if (process.env.MONGO_URL) {
+            console.log('MDB> ', action)
+            Smiles.insert(action)
+            // will change:
+            // Smiles.find().count()
         }
     })
 })
